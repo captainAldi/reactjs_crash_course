84dbaa65fb2cf58d24f0650628f10573f4a3609f
captainAldi <renaldiyulvianda@yahoo.com>
2020-01-27 16:58:18 +0700

    Updates

---
+++ precache-manifest.19be07c6e9e805a42358e507f6675ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c70b42252c2a0f90e9f93284ea987ab",
+    "url": "/captainAldi/reactjs_crash_course.git/index.html"
+  },
+  {
+    "revision": "3e7d78078c230c3902da",
+    "url": "/captainAldi/reactjs_crash_course.git/static/css/main.153622f2.chunk.css"
+  },
+  {
+    "revision": "5818680fc00980d0fca9",
+    "url": "/captainAldi/reactjs_crash_course.git/static/js/2.ae6517d7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/captainAldi/reactjs_crash_course.git/static/js/2.ae6517d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e7d78078c230c3902da",
+    "url": "/captainAldi/reactjs_crash_course.git/static/js/main.6454b14b.chunk.js"
+  },
+  {
+    "revision": "590ce6b8e56db930f999",
+    "url": "/captainAldi/reactjs_crash_course.git/static/js/runtime-main.0b87011f.js"
+  }
+]);
