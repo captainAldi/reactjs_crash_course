fb4a51611ba6eeeac06f3ab5a2958455e83ded5a
captainAldi <renaldiyulvianda@yahoo.com>
2020-01-27 17:16:33 +0700

    Updates

--- precache-manifest.dbb456530fe7c8e1df444b6cd21b3143.js
+++ precache-manifest.dbb456530fe7c8e1df444b6cd21b3143.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c36e1e66e1b31c9962994f9da2e14054",
+    "revision": "167ccd5145f125380885b7f5f51016f3",
     "url": "/reactjs_crash_course/index.html"
   },
   {
-    "revision": "05d85d36cac6bfe384c2",
+    "revision": "91d7f455e18822f86872",
     "url": "/reactjs_crash_course/static/css/main.153622f2.chunk.css"
   },
   {
-    "revision": "5818680fc00980d0fca9",
-    "url": "/reactjs_crash_course/static/js/2.ae6517d7.chunk.js"
+    "revision": "bcb834a4596e76d5e26b",
+    "url": "/reactjs_crash_course/static/js/2.f61ed9aa.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/reactjs_crash_course/static/js/2.ae6517d7.chunk.js.LICENSE"
+    "url": "/reactjs_crash_course/static/js/2.f61ed9aa.chunk.js.LICENSE"
   },
   {
-    "revision": "05d85d36cac6bfe384c2",
-    "url": "/reactjs_crash_course/static/js/main.22e42ab6.chunk.js"
+    "revision": "91d7f455e18822f86872",
+    "url": "/reactjs_crash_course/static/js/main.b53e8aa1.chunk.js"
   },
   {
     "revision": "c5a5ce14e67bdea90408",
